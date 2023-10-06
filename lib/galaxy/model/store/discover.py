172d1cde55f4d35bb80742b6f37135ef44c71e30
Yakubov, Sergey <yakubovs@ornl.gov>
2023-10-06 12:15:31 -0400

    do not set dataset visible to true in case of failing jobs

--- lib/galaxy/model/store/discover.py
+++ lib/galaxy/model/store/discover.py
@@ -145,8 +145,8 @@ class ModelPersistenceContext(metaclass=abc.ABCMeta):
                 self.add_library_dataset_to_folder(library_folder, ld)
                 primary_data = ldda
         primary_data.raw_set_dataset_state(final_job_state)
-        if final_job_state == galaxy.model.Job.states.ERROR and not self.get_implicit_collection_jobs_association_id():
-            primary_data.visible = True
+#        if final_job_state == galaxy.model.Job.states.ERROR and not self.get_implicit_collection_jobs_association_id():
+#            primary_data.visible = True
 
         for source_dict in sources:
             source = galaxy.model.DatasetSource()
