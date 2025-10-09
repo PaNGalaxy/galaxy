34d97b5e37f8580a73be55d561d3c8251839365a
Duggan, John <dugganjw@ornl.gov>
2025-10-09 19:06:01 +0000

    Add NOVA link to header

--- lib/galaxy/managers/configuration.py
+++ lib/galaxy/managers/configuration.py
@@ -124,6 +124,7 @@ class ConfigSerializer(base.ModelSerializer):
             # TODO: this is available from user data, remove
             "is_admin_user": lambda *a, **c: False,
             "brand": _use_config,
+            "dashboard_url": _use_config,
             "logo_url": _use_config,
             "logo_src": _use_config,
             "logo_src_secondary": _use_config,
